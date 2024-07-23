1b4fd7daf1a936f9742577aad05f42d5bfbbcc56
Martin Labrecque <mlabrecque@remarquesystems.com>
2024-07-23 10:04:19 -0400

    trying production key

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   supabaseUrl: 'https://qcavqyndhyyvpjrglusa.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjYXZxeW5kaHl5dnBqcmdsdXNhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMTEzOTMzNSwiZXhwIjoyMDM2NzE1MzM1fQ.xvjA03Ut41-jItuPFB4AlD8g6TA7h0ZHwrTZiD-xVsw',
 }
